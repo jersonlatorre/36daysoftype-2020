c6b93fbef35641e5c92f9b36d9a380e0a2799a4b
Jerson La Torre <jerson.latorre@gmail.com>
2020-03-08 20:01:02 -0500

    code formated

--- F/main.js
+++ F/main.js
@@ -4,28 +4,29 @@ let SETRIPED_IMAGE_SIZE = 2
 let N_FRAMES = 10
 let N_IMAGES = 3
 
+let FACTOR = CANVAS_SIZE / 1080
 let currentImage = 0
 let pointer
 let frames = []
 let scanimages = []
-let striped
+let stripedImage
 let font
 let frame
 let clickSound
 let isStripesVisible = true
-let toggleStripeButtonX = 943 * CANVAS_SIZE / 1080
-let toggleStripeButtonY = 997 * CANVAS_SIZE / 1080
-let toggleStripeButtonRadius = 32 * CANVAS_SIZE / 1080
-let selectImageButtonX = 900 * CANVAS_SIZE / 1080
-let selectImageButtonY = 997 * CANVAS_SIZE / 1080
-let selectImageButtonRadius = 32 * CANVAS_SIZE / 1080
-let loadedCounter = 0
+let toggleStripeButtonX = 943 * FACTOR
+let toggleStripeButtonY = 997 * FACTOR
+let toggleStripeButtonRadius = 32 * FACTOR
+let selectImageButtonX = 900 * FACTOR
+let selectImageButtonY = 997 * FACTOR
+let selectImageButtonRadius = 32 * FACTOR
 
 function preload() {
 	font = loadFont('assets/cocogoose.ttf')
 	pointer = loadImage('assets/hand.svg')
 	frame = loadImage('assets/frame.png')
 	clickSound = loadSound('assets/click.wav')
+
 	for (let n = 0; n < N_IMAGES; n++) {
 		frames[n] = []
 		for (let i = 0; i < N_FRAMES; i++) {
@@ -40,20 +41,19 @@ function setup() {
 	noCursor()
 	textFont(font)
 
-	console.log(frames)
 	for (let n = 0; n < N_IMAGES; n++) {
 		frames[n].forEach((frame) => {
 			frame.resize(CANVAS_SIZE, CANVAS_SIZE)
 		})
 	}
 
-	pointer.resize(50 * CANVAS_SIZE / 1080, 0)
+	pointer.resize(50 * FACTOR, 0)
 	frame.resize(CANVAS_SIZE, CANVAS_SIZE)
 
 	for (let n = 0; n < N_IMAGES; n++) {
 		scanimages[n] = createGraphics(CANVAS_SIZE, CANVAS_SIZE)
 	}
-	striped = createGraphics(SETRIPED_IMAGE_SIZE * CANVAS_SIZE, CANVAS_SIZE)
+	stripedImage = createGraphics(SETRIPED_IMAGE_SIZE * CANVAS_SIZE, CANVAS_SIZE)
 	createBaseImage()
 	createStripedImage()
 }
@@ -64,11 +64,13 @@ function draw() {
 	// current image
 	image(scanimages[currentImage], 0, 0)
 	if (isStripesVisible) {
-		image(striped, mouseX / 5 - SETRIPED_IMAGE_SIZE * CANVAS_SIZE / 2, 0)
+		image(stripedImage, mouseX / 5 - SETRIPED_IMAGE_SIZE * CANVAS_SIZE / 2, 0)
 	}
 
+	// frame
 	image(frame, 0, 0)
 
+	// buttons
 	if (isStripesVisible) {
 		fill(colors.red)
 	} else {
@@ -82,15 +84,17 @@ function draw() {
 	textAlign(CENTER, CENTER)
 	fill(colors.black)
 	noStroke()
-	textSize(22 * CANVAS_SIZE / 1080)
-	text('' + (currentImage + 1), selectImageButtonX, selectImageButtonY - 4 * CANVAS_SIZE / 1080)
+	textSize(22 * FACTOR)
+	text('' + (currentImage + 1), selectImageButtonX, selectImageButtonY - 4 * FACTOR)
 
+	// cursor
 	image(pointer, mouseX, mouseY)
 
+	// name
 	textAlign(LEFT)
 	fill(255, 120)
-	textSize(27 * CANVAS_SIZE / 1080)
-	text('Scanimator 1.0', 120 * CANVAS_SIZE / 1080, 990 * CANVAS_SIZE / 1080)
+	textSize(27 * FACTOR)
+	text('Scanimator 1.0', 120 * FACTOR, 990 * FACTOR)
 }
 
 function createBaseImage() {
@@ -115,12 +119,11 @@ function createBaseImage() {
 
 function createStripedImage() {
 	let nStripes = parseInt(SETRIPED_IMAGE_SIZE * CANVAS_SIZE / STRIP_WIDTH)
-	console.log(nStripes)
 	for (let i = 0; i < nStripes; i++) {
-		striped.fill(colors.black)
-		striped.noStroke()
+		stripedImage.fill(colors.black)
+		stripedImage.noStroke()
 		if (i % N_FRAMES != 0) {
-			striped.rect(i * STRIP_WIDTH, 0, STRIP_WIDTH, CANVAS_SIZE)
+			stripedImage.rect(i * STRIP_WIDTH, 0, STRIP_WIDTH, CANVAS_SIZE)
 		}
 	}
 }
